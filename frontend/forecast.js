61f0f93f6d81f2112cae3426e0d25cf1df03e9c1
Hydroxa <alexgillott489@gmail.com>
2021-10-07 11:54:55 +0100

    Added forecast, removed test file

--- frontend/forecast.js
+++ frontend/forecast.js
@@ -1,17 +1,68 @@
-const sites = [];
+const sites = {};
 
 window.onload = function () {
-	console.log("Getting names from server..");
-	fetch("http://localhost:3000/fcsSites").then((response) => {
-		const locationData = response.json();
-		const locations = [];
-		for (let i = 0; i < locationData.length; i++) {
-			locations[i].value = locationData[i].name;
-			sites.push(locationData[i]);
-		}
-		// let formInput = document.querySelector("input[name='location']");
-		// formInput.list = locations;
-	});
+	fetch(`/fcsSites`)
+		.then((response) => response.json())
+		.then((obj) => {
+			const locationData = obj;
+			for (let i = 0; i < locationData.length; i++) {
+				sites[locationData[i].name] = locationData[i];
+			}
+			console.log(sites);
+
+			const today = new Date();
+			const tomorrow = new Date(today);
+			tomorrow.setDate(today.getDate() + 1);
+
+			const labelTmrw = document.getElementById("labelDateTmrw");
+			const labelToday = document.getElementById("labelDateToday");
+			labelTdy.innerHTML = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+			labelTmrw.innerHTML = `${tomorrow.getDate()}/${tomorrow.getMonth() + 1}/${tomorrow.getFullYear()}`;
+
+			document.getElementById("txtLocation").value = "London";
+			getWeatherData();
+		});
 };
 
-function getWeatherData() {}
+function getWeatherData() {
+	const txtInput = document.getElementById("txtLocation").value;
+	const cbxInput = document.getElementById("cbxHourly").value;
+	document.getElementById("labelLocErr").innerHTML = "";
+	if (sites[txtInput] !== undefined) {
+		fetch(`/fcsSite?id=${sites[txtInput].id}&res=${cbxInput ? "3" : "1"}`)
+			.then((response) => response.json())
+			.then((obj) => {
+				const tdy = obj.values[0][2];
+				const tmrw = obj.values[1][2];
+
+				const labelTempTdy = document.getElementById("labelTempToday");
+				const labelTempTmrw = document.getElementById("labelTempTmrw");
+				const labelFeelsTdy = document.getElementById("labelFeelsToday");
+				const labelFeelsTmrw = document.getElementById("labelFeelsTmrw");
+				const labelWindDirTdy = document.getElementById("labelWindDirToday");
+				const labelWindDirTmrw = document.getElementById("labelWindDirTmrw");
+				const labelWindSpdTdy = document.getElementById("labelWindSpeedToday");
+				const labelWindSpdTmrw = document.getElementById("labelWindSpeedTmrw");
+				const labelRainChanceTdy = document.getElementById("labelRainChanceToday");
+				const labelRainChanceTmrw = document.getElementById("labelRainChanceTmrw");
+
+				labelTempTdy.innerHTML = `Temperature: ${tdy["T"]}C`;
+				labelTempTmrw.innerHTML = `Temperature: ${tmrw["T"]}C`;
+				labelFeelsTdy.innerHTML = `Feels like: ${tdy["F"]}C`;
+				labelFeelsTmrw.innerHTML = `Feels like: ${tmrw["F"]}C`;
+
+				labelWindSpdTdy.innerHTML = `Wind Speed: ${tdy["W"]}mph`;
+				labelWindSpdTmrw.innerHTML = `Wind Speed: ${tmrw["W"]}mph`;
+
+				const windDirTdy = tdy["D"].replaceAll("S", "South ").replaceAll("W", "West ").replaceAll("N", "North ").replaceAll("E", "East ");
+				const windDirTmrw = tmrw["D"].replaceAll("S", "South ").replaceAll("W", "West ").replaceAll("N", "North ").replaceAll("E", "East ");
+				labelWindDirTdy.innerHTML = `Wind Direction: ${windDirTdy}`;
+				labelWindDirTmrw.innerHTML = `Wind Direction: ${windDirTmrw}`;
+
+				labelRainChanceTdy.innerHTML = `Rain Chance: ${tdy["Pp"]}%`;
+				labelRainChanceTmrw.innerHTML = `Rain Chance: ${tmrw["Pp"]}%`;
+			});
+	} else {
+		document.getElementById("labelLocErr").innerHTML = "That location is not available";
+	}
+}
